6fdc59fc706510609b6c34e2aa81294e6c1015b3
Kristian <49764796+kristianka@users.noreply.github.com>
2023-08-27 16:33:36 +0300

    updatr blogs route

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,13 +2,14 @@ const router = require('express').Router()
 const { Blog } = require('../models')
 const { User } = require('../models')
 const { tokenExtractor } = require('../util/middlewares')
+const jwt = require('jsonwebtoken')
 
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll({
         attributes: { exclude: ['userId'] },
         include: {
             model: User,
-            attributes: ['name']
+            attributes: ['username', 'name']
         }
     })
     res.json(blogs)
@@ -16,7 +17,6 @@ router.get('/', async (req, res) => {
 
 router.post('/', tokenExtractor, async (req, res, next) => {
     try {
-        console.log(req.body)
         const user = await User.findByPk(req.decodedToken.id)
         const blog = await Blog.create({ ...req.body, userId: user.id, date: new Date() })
         res.json(blog)
@@ -25,14 +25,18 @@ router.post('/', tokenExtractor, async (req, res, next) => {
     }
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', tokenExtractor, async (req, res, next) => {
     try {
         const blog = await Blog.findByPk(req.params.id)
         if (!blog) {
             return res.status(404).json({ error: "blog not found" })
         }
+        const user = await User.findByPk(req.decodedToken.id)
+        if (user.id !== blog.userId) {
+            return res.status(401).json({ error: "unauthorized" })
+        }
         await blog.destroy()
-        res.status(204).send("success")
+        res.status(204).json({ message: "blog deleted" })
     } catch (error) {
         next(error)
     }
--- util/middlewares.js
+++ util/middlewares.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
     return res.status(400).json({ error })
@@ -7,8 +8,9 @@ const tokenExtractor = (req, res, next) => {
     const authorization = req.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
         try {
-            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-        } catch {
+            req.decodedToken = jwt.verify(authorization.substring(7), process.env.SECRET)
+        } catch (err) {
+            console.log(err)
             return res.status(401).json({ error: 'token invalid' })
         }
     } else {
